47569b482ae78fa0259b8450ed9c7e5c78cb86d6
jauntywunderkind <jaunty+wunder+kind+git@eldergods.com>
2019-07-11 19:59:37 -0400

    Make immediate test test interleaving.
    Which, as per last commit, I'm not sure is really totally fair? I mean...
    I think it might be... there ought be a flush of the task queue that ping
    pongs, I think, between the two readIntos, but I'm not fully sure that's
    absolutely positively the case, but I do think it might be. Anyhow, like
    this test for now. Wfm?

--- .test/immediate.js
+++ .test/immediate.js
@@ -3,8 +3,11 @@ import Immediate from "../immediate.js"
 import tape from "tape"
 
 tape( "test that immediate eventually returns everything", async function( t){
+	t.plan( 14)
 	async function readInto( arr, n= 3){
 		for await( const val of Immediate( n)){
+			t.equal( arr, expectSource.shift(), `interleaved array ${ arr=== vals1? "1": "2"} yielded`)
+			t.equal( val, expectVals.shift(), "interleaved read")
 			arr.push( val)
 		}
 		return arr
@@ -12,17 +15,13 @@ tape( "test that immediate eventually returns everything", async function( t){
 	const
 	  vals1= [],
 	  vals2= [],
+	  expectSource= [ vals1, vals2, vals1, vals2, vals1, vals2],
+	  expectVals= [ 0, 0, 1, -1, 2, -2],
 	  reads1= readInto( vals1, 3),
 	  reads2= readInto( vals2, -3),
 	  read1= await reads1,
 	  read2= await reads2
 	t.equal( read1.length, 3, "first read hapepned")
 	t.equal( read2.length, 3, "second read happened")
-	t.equal( read1[ 0], 0, "read1 is 0")
-	t.equal( read1[ 1], 1, "read1 is 1")
-	t.equal( read1[ 2], 2, "read1 is 2")
-	t.equal( read2[ 0], 0, "read1 is 0")
-	t.equal( read2[ 1], -1, "read1 is -1")
-	t.equal( read2[ 2], -2, "read1 is -2")
 	t.end()
 })
